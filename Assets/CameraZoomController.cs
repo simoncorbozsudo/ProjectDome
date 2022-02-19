9c59e9981c117e37726c24a08e1a7bc62ff66187
simon.corboz <simon.corboz@edu.hefr.ch>
2022-02-19 12:29:44 +0100

    added back and rear cameras

---
+++ Assets/CameraZoomController.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraZoomController : MonoBehaviour
+{
+    public float minZoom;
+    public float maxZoom;
+    private Camera cam;
+    private float targetZoom;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
